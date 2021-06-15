94dd81f52932a68b196b561a6497536fc0c06b00
Andrew Calder <andrew.calder@uqconnect.edu.au>
2021-06-15 17:32:02 +1000

    FIX: Fix nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :trails, only: [:index]
+  resources :mountains, only: [:index] do
+    resources :trails, only: [:index]
+  end
+
+  resources :trails, only: [:show] do
+    resources :reviews, only: [:create]
+  end
 end
