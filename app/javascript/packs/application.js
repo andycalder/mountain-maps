9c62cc1753e46c661accc08881ad9b910a31d58b
Andrew Calder <andrew.calder@uqconnect.edu.au>
2021-06-16 12:29:23 +1000

    ARCH: Implemented Turbo frames prototype, upgraded Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,10 +26,9 @@ import { Turbo, cable } from '@hotwired/turbo-rails'
 // External imports
 import "bootstrap";
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+// Internal imports
+import { initMapbox } from '../plugins/init_mapbox';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+document.addEventListener('turbo:load', () => {
+  initMapbox();
 });
