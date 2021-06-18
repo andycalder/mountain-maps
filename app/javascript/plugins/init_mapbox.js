3c957e9151e4115f775b2d36e4f250c9cbb88a15
Andrew Calder <andrew.calder@uqconnect.edu.au>
2021-06-18 13:03:26 +1000

    FIX: Fix mapbox css

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,5 @@
 import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+//import 'mapbox-gl/dist/mapbox-gl.css';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
