0a92c376535e9247afeb7f2028ca4734e1b1b15c
Hongleang Lim <lim.hongleang@gmail.com>
2021-06-23 15:07:22 +1000

    STYLE: update loader style

--- app/javascript/controllers/disable_onclick_controller.js
+++ app/javascript/controllers/disable_onclick_controller.js
@@ -1,28 +1,24 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static values = {
-    disabledText: String
-  }
 
   connect() {
   }
 
   disable(e) {
     // prevent the click event
-    // e.preventDefault();
+    e.preventDefault();
     console.log("clicked");
     
     this.element.disabled = true; // disable the button after the user click
-    if (this.disabledTextValue) {
-      this.element.innerHTML = `<div class="loading">
-                                  <div class="lds-ellipsis">
-                                    <div></div>
-                                    <div></div>
-                                    <div></div>
-                                    <div></div>
-                                  </div>
-                                </div>`
-    }
+    this.element.innerHTML = `<div class="loading">
+                                <div class="lds-ellipsis">
+                                  <div></div>
+                                  <div></div>
+                                  <div></div>
+                                  <div></div>
+                                </div>
+                              </div>`
+    
   }
 }
