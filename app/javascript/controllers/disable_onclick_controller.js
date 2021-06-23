2a2f0586c0f8001172304c7ac4bbb1ee56d3b412
Hongleang Lim <lim.hongleang@gmail.com>
2021-06-23 15:11:03 +1000

    FIX: deactivate e.preventDefault()

--- app/javascript/controllers/disable_onclick_controller.js
+++ app/javascript/controllers/disable_onclick_controller.js
@@ -7,8 +7,8 @@ export default class extends Controller {
 
   disable(e) {
     // prevent the click event
-    e.preventDefault();
-    console.log("clicked");
+    // e.preventDefault();
+    // console.log("clicked");
     
     this.element.disabled = true; // disable the button after the user click
     this.element.innerHTML = `<div class="loading">
