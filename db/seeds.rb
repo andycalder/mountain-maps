8f9b92f411efff535f44ee703536a030344283e4
Andrew Calder <andrew.calder@uqconnect.edu.au>
2021-06-24 12:19:43 +1000

    DATA: Seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,46 @@
 puts 'Seeding...'
 
+users = {
+  'andrew': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495193/avatars/andrew_lcwitq.png',
+  'paal': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495196/avatars/paal_gavt7r.png',
+  'sy': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495196/avatars/sy_gjnjn7.png',
+  'shank': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495196/avatars/shank_qbmch8.png',
+  'marty': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495195/avatars/marty_nrtgmk.png',
+  'matt': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495195/avatars/matt_dcapcf.png',
+  'sheila': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495195/avatars/sheila_cxz4wt.png',
+  'hong': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495195/avatars/hong_ghqsoo.png',
+  'cam': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495195/avatars/cam_org2rk.png',
+  'hillel': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495194/avatars/hillel_w3l3l0.png',
+  'george': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495195/avatars/george_fs87by.png',
+  'julain': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495194/avatars/julian_hyjn5i.png',
+  'cass': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495194/avatars/cass_ctqyrg.png',
+  'manon': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495194/avatars/manon_jrckrv.png',
+  'jorge': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495194/avatars/jorge_kutokl.png',
+  'julia': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624495193/avatars/julia_c5vz5m.jpg',
+  'bobby': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624500280/avatars/bobby_nzvsur.jpg',
+  'hardcorebikrrr': 'https://res.cloudinary.com/daysaku3r/image/upload/v1624500496/avatars/hardcorebikrrr_gw6fod.jpg'
+}
+
+users.each do |nickname, photo_url|
+  file = URI.open(photo_url)
+  user = User.new(email: "#{nickname}@gmail.com", password: 'secret', nickname: nickname)
+  user.photo.attach(io: file, filename: "#{nickname}@png", content_type: 'image/png')
+
+  if user.valid?
+    user.save
+  else
+    p 'Unable to save user'
+  end
+end
+
+whistler = Mountain.create!(name: 'Whistler')
+
 dataset_id = 'cj51y5ioi09wh32p1s9frlt9l'
 access_token = ENV['MAPBOX_ACCESS_TOKEN']
 dataset_url = "https://api.mapbox.com/datasets/v1/andycalder/#{dataset_id}/features?access_token=#{access_token}"
 
 dataset = URI.open(dataset_url).read
 trails = JSON.parse(dataset)['features']
-
-whistler = Mountain.create!(name: 'Whistler')
-admin_user = User.create!(email: 'admin@gmail.com', password: 'secret', nickname: 'admin')
 
 def difficulty(color)
   case color
